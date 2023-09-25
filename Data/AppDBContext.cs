d0ab17b314a9c757c9da0dcbd6c9f73f52d8f078
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-09-25 11:29:43 +0300

    Formating

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace E_Commerce.Data
 {
     using E_Commerce.Models;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using System;
@@ -8,7 +9,7 @@
 
     public class AppDbContext : IdentityDbContext<User>
     {
-        public AppDbContext(){}
+        public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         { }
@@ -24,6 +25,19 @@
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            modelBuilder.Entity<IdentityRole>().HasData(seedRoles());
+
+            modelBuilder.Entity<User>().HasData(seedSuperAdmin());
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "1", // RoleId for SuperAdmin
+                    UserId = "1" // User Id for the default user
+                }
+            );
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -32,6 +46,45 @@
             var Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var connectionString = Configuration.GetConnectionString("local");
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        private List<IdentityRole> seedRoles()
+        {
+            return new List<IdentityRole> {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "SuperAdmin",
+                    NormalizedName = "SUPERADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "3",
+                    Name = "Customer",
+                    NormalizedName = "CUSTOMER"
+                }
+            };
+        }
+        private User seedSuperAdmin()
+        {
+            var hasher = new PasswordHasher<IdentityUser>();
+            return new User
+            {
+                Id = "1",
+                UserName = "TemporaryUsername",
+                NormalizedUserName = "TEMPORARY-USERNAME",
+                Email = "TemporaryEmail@example.com",
+                NormalizedEmail = "TEMPORARYEMAIL@EXAMPLE.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "TemporaryPassword"),
+                SecurityStamp = string.Empty
+            };
         }
     }
 
